583e2621344b67213c1cacbee854e492444c5449
sumergo <sambcramer@gmail.com>
2021-06-02 22:17:24 -0700

    Practicing

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,9 +7,45 @@ class StudentsController < ApplicationController
     @Student = Student.find(params[:id])
   end
 
+  def edit
+    @Student = Student.find(params[:id])
+  end
+
   def update
+    @Student = Student.find(params[:id])
+
+    if @Student.update(student_params)
+      redirect_to @Student
+    else
+      render :edit
+    end
   end
   
   def destroy
+    @Student = Student.find(params[:id])
+    @Student.destroy
+
+    redirect_to '/index'
+  end
+
+  def new
+    @Student = Student.new
+  end
+  
+  def create
+    @Student = Student.new(student_params)
+    @Student.check_in_time = params[:created_at]
+    if @Student.save
+      redirect_to '/index'
+      
+    else
+      render :new
+      
+    end
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:first_name,:last_name,:graduation_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   resources :students
   get '/index', to: 'students#index'
+  get '/new', to: 'students#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
